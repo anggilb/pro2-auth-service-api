3e875c769314a43911c5c6a92179f596c78e456e
Ángel Javier Gil Bécares <angeljavgil@gmail.com>
2025-01-18 19:44:00 +0100

    Kick off.

---
+++ src/main/java/com/example/auth_service_api/controller/impl/AuthController.java
@@ -0,0 +1,22 @@
+package com.example.auth_service_api.controller.impl;
+
+import com.example.auth_service_api.commons.dtos.TokenResponse;
+import com.example.auth_service_api.commons.dtos.UserRequest;
+import com.example.auth_service_api.controller.AuthApi;
+import com.example.auth_service_api.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController implements AuthApi {
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @Override
+    public ResponseEntity<TokenResponse> createUser(UserRequest userRequest) {
+        return ResponseEntity.ok(authService.createUser(userRequest));
+    }
+}
---
+++ src/main/java/com/example/auth_service_api/service/AuthService.java
@@ -0,0 +1,8 @@
+package com.example.auth_service_api.service;
+
+import com.example.auth_service_api.commons.dtos.TokenResponse;
+import com.example.auth_service_api.commons.dtos.UserRequest;
+
+public interface AuthService {
+    TokenResponse createUser(UserRequest userRequest);
+}
