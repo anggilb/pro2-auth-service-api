69378411e0b6b3a286ea9f74a87607b20374e93a
Ángel Javier Gil Bécares <angeljavgil@gmail.com>
2025-01-19 23:38:15 +0100

    Usando Header Authorization con Swagger.

--- src/main/java/com/example/auth_service_api/config/SwaggerConfig.java
+++ src/main/java/com/example/auth_service_api/config/SwaggerConfig.java
@@ -1,23 +1,18 @@
 package com.example.auth_service_api.config;
 
-
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
 
+/**
+ * The type Swagger config.
+ */
 @Configuration
+@SecurityScheme(
+        name = "Bearer Authorization",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
 public class SwaggerConfig {
-
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-                .components(new Components().addSecuritySchemes("bearerAuth", new SecurityScheme()
-                        .type(SecurityScheme.Type.HTTP)
-                        .scheme("bearer")
-                        .bearerFormat("JWT")));
-    }
 }
